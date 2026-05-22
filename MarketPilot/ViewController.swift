3ab4e7d6c15786c715eece91d0105faa6b6d0c50
omerkmk03 <omerkumek03@gmail.com>
2026-05-22 14:39:27 +0300

    Initial Commit

---
+++ MarketPilot/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarketPilot
+//
+//  Created by ömer kumek on 22.05.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MarketPilotTests/MarketPilotTests.swift
@@ -0,0 +1,17 @@
+//
+//  MarketPilotTests.swift
+//  MarketPilotTests
+//
+//  Created by ömer kumek on 22.05.2026.
+//
+
+import Testing
+@testable import MarketPilot
+
+struct MarketPilotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
